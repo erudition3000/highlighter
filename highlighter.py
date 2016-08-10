5e7b106b1944f52d4c16809331938469d32224ac
erudition3000 <bryce.cooper@gmail.com>
2016-08-10 18:03:03 -0400

    version 1.0
    I think all basic functionality is included now.

--- highlighter.py
+++ highlighter.py
@@ -1,37 +1,35 @@
+#!/usr/bin/python
 import argparse, re, sys
 from xml.etree import cElementTree as ET
-#import cElementTree as ET
 
-
-#colors are 0:black, 1:red, 2:green, 3:yellow, 4:blue, 5: magenta, 6:cyan, 7:white
-#Font colors are in the 30s and background colors are in the 40s
-#Add '1; before the color to make it 'bright intensity'
-#
-#This might help. Run the command: echo -e "testing \033[0;37;41mCOLOR1\033[1;35;44mCOLOR2\033[m"
-#This should output testing COLOR1COLOR2 with COLOR1 being white (light gray) text on a red background, and COLOR2 being bold / bright magenta text on a blue background. All subsequent text should be normally colored.
+#Looks at each line of stdin and adds ANSI color escape sequences to regular expressions
+#defined on the comand-line or in a config file.  To specify colors use standard color 
+#names: black, red, green, yellow, blue, magenta, cyan, white
 
 class HighlightSearch():
-    defaultForeground = '0'	#black
-    defaultBackground = '3'	#yellow
-    defaultBrightness = '0'	#Dark
-    startTagTemplate = '\e[{0};3{1};4{2}m'
-    endTag = r'\e[0m'	#normal
+    defaultForeground = '0'     #black
+    defaultBackground = '3'     #yellow
+    defaultBrightness = '0'     #Dark
+    startTagFgBgTemplate = '\e[{0};3{1};4{2}m'
+    startTagFgTemplate = '\e[{0};3{1}m'
+    startTagBgTemplate = '\e[4{0}m'
+    endTag = r'\e[0m'   #back to terminal default
     colorMap = {'black': '0', 'red':'1', 'green':'2', 'yellow':'3', 'blue':'4', 'magenta':'5', 'cyan':'6', 'white':'7'}
 
-    def __init__(self, searchPat):
+    def __init__(self, searchPat, fg=None, bg=None):
         self.brightColor = self.defaultBrightness
+        self.foreground = None
+        self.background = None
         
         try:
-            self.searchPat = re.compile(searchPat[0])
+            self.searchPat = re.compile(searchPat)
             self.compileError = False
         except re.error, e:
-            sys.stderr.write('ERROR: Invalid regex "{0}"\n    {1}\n'.format(searchPat[0], e))
+            sys.stderr.write('ERROR: Invalid regex "{0}"\n    {1}\n'.format(searchPat, e))
             self.compileError = True
             
-        if len(searchPat)<2:
-            self.foreground = self.defaultForeground
-        else:
-            fg = searchPat[1].lower()
+        if fg:
+            fg = fg.lower()
             if 'light-' in fg:
                 self.brightColor = 1
                 fg = fg.replace('light-','')
@@ -39,28 +37,35 @@ class HighlightSearch():
                 self.foreground = self.colorMap[fg]
             else:
                 self.foreground = self.defaultForeground
-                sys.stderr.write('ERROR: Invalid foreground color "{0}"\n'.format(searchPat[1]))
+                sys.stderr.write('ERROR: Invalid foreground color "{0}"\n'.format(fg))
                 self.compileError = True
+        elif bg is None:    #if no FG or BG then set default FG
+            self.foreground = self.defaultForeground
         
-        if len(searchPat)<3:
-            self.background = self.defaultBackground
-        else:
-            bg = searchPat[2].lower()
+        if bg:
+            bg = bg.lower()
             if self.colorMap.has_key(bg):
                 self.background = self.colorMap[bg]
             else:
                 self.background = self.defaultBackground
-                sys.stderr.write('ERROR: Invalid background color "{0}"\n'.format(searchPat[2]))
+                sys.stderr.write('ERROR: Invalid background color "{0}"\n'.format(bg))
                 self.compileError = True
+        elif fg is None:        #if no FG or BG then set default BG
+            self.background = self.defaultBackground
             
     def isError(self):
         return self.compileError
     
     def search(self, text):
-       startTag = self.startTagTemplate.format(self.brightColor, self.foreground, self.background)
-       (newText,numChanges) = self.searchPat.subn('{0}\g<0>{1}'.format(startTag,self.endTag), text)
-       foundMatch = True if numChanges>0 else False
-       return (foundMatch, newText)
+        if self.foreground and self.background:
+            startTag = self.startTagFgBgTemplate.format(self.brightColor, self.foreground, self.background)
+        elif self.foreground:
+            startTag = self.startTagFgTemplate.format(self.brightColor, self.foreground)
+        else:
+            startTag = self.startTagBgTemplate.format(self.background)
+        (newText,numChanges) = self.searchPat.subn('{0}\g<0>{1}'.format(startTag,self.endTag), text)
+        foundMatch = True if numChanges>0 else False
+        return (foundMatch, newText)
         
     
     
@@ -87,7 +92,10 @@ def parseCommandLine():
             
     if not parseError and args.search:
         for searchArg in args.search:
-            hls = HighlightSearch(searchArg)
+            searchPat = searchArg[0]
+            fg = searchPat[1] if len(searchArg)>1 else None
+            bg = searchPat[2] if len(searchArg)>2 else None
+            hls = HighlightSearch(searchPat, fg, bg)
             if not hls.compileError:
                 searches.append(hls)
             else:
@@ -107,7 +115,6 @@ def parseConfigurationFile(configFile):
     parseError = False
     
     try:
-        print 'Parsing',configFile
         dom = ET.parse(open(configFile, 'r'))
         root = dom.getroot()
         for searchTag in root.findall('search'):
@@ -115,15 +122,10 @@ def parseConfigurationFile(configFile):
             searchPat = searchTag.text
             if searchTag.attrib.has_key('fg'):
                 fg = searchTag.attrib['fg']
-                if searchTag.attrib.has_key('bg'):
-                    bg = searchTag.attrib['bg']
+            if searchTag.attrib.has_key('bg'):
+                bg = searchTag.attrib['bg']
             
-            searchArg = [searchPat]
-            if fg:
-                searchArg.append(fg)
-            if bg:
-                searchArg.append(bg)
-            hls = HighlightSearch(searchArg)
+            hls = HighlightSearch(searchPat, fg, bg)
             if not hls.compileError:
                 searches.append(hls)
             else:
